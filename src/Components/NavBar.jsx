94340f88072e61f3ee6dbc9fbce771b246754992
Nishant Thakur <nishantthakur1357@gmail.com>
2021-10-28 22:03:56 +0530

    added favorite function

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import logo from "../images/logo.png";
+import data from './data'
 
-
-const NavBar = () => {
+const NavBar = ({favFoodList,toggle,setToggle,setItems}) => {
   const displayFav=()=>{
-
+  setToggle(!toggle);
+  document.querySelector(".NavHeart").classList.toggle("red")
+  
+  if(toggle)
+  {
+    setItems(favFoodList);
+    
+  }
+  else{
+    setItems(data);
+  
+  }
   }
   return (
     <nav className="navbar">
@@ -13,7 +24,9 @@ const NavBar = () => {
         <img src={logo} />
       </div>
       <div className="right">
-        <span className="NavHeart"><AiOutlineHeart /></span>
+        <span className="NavHeart"><AiOutlineHeart 
+         onClick={displayFav}
+        /></span>
         <AiOutlineShoppingCart />
       </div>
     </nav>
